d97c414b921f2da79f2c2e396a59ff3a7fa3f736
Shubha Rajan <shubharajan94@gmail.com>
2019-03-30 19:05:00 -0700

    added validation to query model

--- app/models/query.rb
+++ app/models/query.rb
@@ -1,4 +1,7 @@
 class Query < ApplicationRecord
   has_and_belongs_to_many :genders
   has_and_belongs_to_many :ethnicities
+
+  validates :consent, acceptance: true
+  validates :username, presence: true
 end
